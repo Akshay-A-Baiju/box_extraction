c97c277ce95ebdda20e4b93ce95594d4371994c1
Akshay A Baiju <akshaydgp2015@gmail.com>
2024-02-08 00:33:21 +0530

    added contours & sorted them

--- box_detection.py
+++ box_detection.py
@@ -3,6 +3,28 @@ import numpy as np
 
 image_path = "img\src\\43.png"
 
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+
+    # construct the list of bounding boxes and sort them from top to bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
+
 def box_extraction(original_img_path, cropped_dir_path):
 
     print("Reading image...")
@@ -50,5 +72,13 @@ def box_extraction(original_img_path, cropped_dir_path):
     # See vertical and horizontal lines in the image which is used to find boxes
     print("Image which only contains boxes: img/output/img_final_bin.jpg")
     cv2.imwrite("img/output/img_final_bin.jpg",img_final_bin)
+
+    # Find contours for image, which will detect all the boxes
+    contours, hierarchy = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Sort all the contours by top to bottom.
+    (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
+    # # Printing contours
+    # cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
+    # cv2.imwrite("./temp/img_contour.jpg", img)
 
 box_extraction(image_path,"./img/output/")
