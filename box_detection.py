ef764f42506a0e416c5ad5023dc738354d61f63b
Akshay A Baiju <akshaydgp2015@gmail.com>
2024-02-08 00:00:58 +0530

    added vertical kernel

--- box_detection.py
+++ box_detection.py
@@ -17,4 +17,14 @@ def box_extraction(original_img_path, cropped_dir_path):
     print("Storing binary image to img/output/image_bin.jpg")
     cv2.imwrite("img/output/image_bin.jpg",img_bin)
 
+    # Vertical kernel
+    vertical_kernel_length=np.array(img).shape[1]//20
+    print("vertical kernel length : ",vertical_kernel_length)
+    # A verticle kernel of (1 X vertical_kernel_length), which will detect all the vertical lines from the image.
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, vertical_kernel_length))
+    # Morphological operation to detect vertical lines from an image
+    img_temp1 = cv2.erode(img_bin, vertical_kernel, iterations=3)
+    vertical_lines_img = cv2.dilate(img_temp1, vertical_kernel, iterations=3)
+    cv2.imwrite("img/output/vertical_lines.jpg",vertical_lines_img)
+
 box_extraction(image_path,"./img/output/")
